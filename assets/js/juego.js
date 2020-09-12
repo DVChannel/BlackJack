276f484277c2a5a7ebbf91981039ce7841977533
DVChannel <channelgonzalezjimenez100gmail.com>
2020-09-11 19:36:11 -0500

    v3

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,7 +1,7 @@
 let deck = [ ];
 const tipos= ['C','D','H','S'];
 const especiales= ['A','J','Q','K'];
-
+//crea baraja
 const crearDeck = () => {
 for(let i=2; i<=10; i++){
     for (let tipo of tipos){
@@ -13,10 +13,24 @@ for(let tipo of tipos){
     deck.push(esp + tipo);
 }
 }
-console.log( deck );
+//console.log( deck );
 deck = _.shuffle( deck );
 console.log( deck );
 return deck;
 
 }
 crearDeck( );
+//tomar carta
+const pedirCarta = () =>{
+ 
+    if(deck.length===0){
+        throw 'No hay cartas en deck';
+    }
+
+    const carta= deck.pop();
+    console.log(deck);
+    console.log(carta);
+    return carta;
+}
+
+// pedirCarta();
