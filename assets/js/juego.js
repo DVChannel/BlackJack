d9892a86fb3a9d7823056868a3774009f5e08e0d
DVChannel <channelgonzalezjimenez100gmail.com>
2020-09-11 22:53:50 -0500

    v8

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -3,10 +3,11 @@ const tipos= ['C','D','H','S'];
 const especiales= ['A','J','Q','K'];
 
 const newg = document.querySelector('#new');
-const stop = document.querySelector('#stop');
+const bstop = document.querySelector('#bstop');
 const pedir = document.querySelector('#pedir');
 
 const divCJugador = document.querySelector('#jugador');
+const divCP = document.querySelector('#computadora');
 
 let puntosJugador = 0, puntosComputadora = 0;
 const smalls = document.querySelectorAll('small');
@@ -29,6 +30,7 @@ return deck;
 
 }
 crearDeck();
+
 //tomar carta
 const pedirCarta = () =>{
  
@@ -44,6 +46,25 @@ const valor = carta.substring(0, carta.length - 1);
 return (isNaN(valor)) ?
 (valor ==='A')? 11 : 10
 : valor * 1;
+}
+
+
+const turnoComputadora = ( puntosJugador)=>{
+
+    do{
+
+        const carta = pedirCarta();
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        smalls [1].innerText= puntosComputadora; 
+        
+        const imgcarta = document.createElement('img');
+        imgcarta.src = `assets/cartas/${carta}.png` ;
+        imgcarta.classList.add('carta');
+        
+        divCP.append(imgcarta);
+        if (puntosMinimos>21){break;}
+    }while((puntosComputadora<puntosMinimos) && (puntosMinimos<=21));
+
 }
 
 //eventos
@@ -62,10 +83,20 @@ divCJugador.append(imgcarta);
 if ( puntosJugador>21){
     console.warn('Perdiste u.u');
     pedir.disabled = true;
+    bstop.disabled = true;
+    turnoComputadora(puntosJugador);
 }else if (puntosJugador === 21 ){
     console.warn('21 uwu');
     pedir.disabled = true;
+    bstop.disabled = true;
+    turnoComputadora(puntosJugador);
 }
 
 
+});
+
+bstop.addEventListener('click',()=>{
+    pedir.disabled = true;
+    bstop.disabled = true;
+turnoComputadora(puntosJugador);
 })
