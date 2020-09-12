6a644fa52fe936b39125f22a1a38002ae1fabbd3
DVChannel <channelgonzalezjimenez100gmail.com>
2020-09-11 20:15:05 -0500

    v5

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -32,5 +32,13 @@ const pedirCarta = () =>{
     console.log(carta);
     return carta;
 }
+const valorCarta = (carta) => {
+const valor = carta.substring(0, carta.length - 1);
+return (isNaN(valor)) ?
+(valor ==='A')? 11 : 10
+: valor * 1;
+}
 
-// pedirCarta();
+
+const valor = valorCarta(pedirCarta());
+console.log({valor});
